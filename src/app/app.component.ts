9e7328e957ebc16d41ce31470950e7884a3930c0
BrentGrammer <brentmarquez@gmail.com>
2019-08-23 13:57:32 -0600

    add custom events and prop binding to child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{ name: "server", type: "Testserver", content: "test" }];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
